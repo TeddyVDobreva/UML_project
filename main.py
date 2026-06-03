91ca81da46e80cd0a1a7168140a5a58b239c9aa2
Teodora Veselinova Dobreva T.V.Dobreva@student.rug.nl <s5888441@omni16.internal.hpc.rug.nl>
2026-06-03 21:05:45 +0200

    typehints and docstrings for train.py and main.py update

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
+from data_analysis import do_analysis
 from preprocessing import preprocess
 from train import train_loop
+
+EPOCHS = 200
+BATCH_SIZE = 128
+LR = 0.1
+MOMENTUM = 0.9
+NESTEROV = True
+DECAY = 5e-4
+PRINT_FREQ = 10
+LAYERS = 40
+WIDE_LAYERS = 2
+DROPRATE = 0
+NAME = "WideResNet-40-2"
+NUM_CLASSES = 22
+LOGNORM_TEMP = 1
 
 
 def main():
     # Preliminaru data analysis
-    # do_analysis("datasets/sea_creatures")
-    # do_analysis("datasets/reptiles")
-    # print("Analysis done!")
+    do_analysis("datasets/sea_creatures")
+    do_analysis("datasets/reptiles")
+    print("Analysis done!")
     # Preprocessing
     sea_creatures_split = preprocess("datasets/sea_creatures")
-    # reptiles_split = preprocess("datasets/reptiles")
+    reptiles_split = preprocess("datasets/reptiles")
     print("Preprocessing done!")
 
     # train the models
@@ -18,7 +33,20 @@ def main():
         sea_creatures_split[3],
         sea_creatures_split[1],
         sea_creatures_split[4],
-        "cross-entropy",
+        loss="cross-entropy",
+        num_classes=NUM_CLASSES,
+        model_name=NAME,
+        num_layers=LAYERS,
+        num_wide_layers=WIDE_LAYERS,
+        droprate=DROPRATE,
+        lr=LR,
+        decay=DECAY,
+        optimizer_momentum=MOMENTUM,
+        nesterov=NESTEROV,
+        lognorm_temperature=LOGNORM_TEMP,
+        batch_size=BATCH_SIZE,
+        epochs=EPOCHS,
+        print_freq=PRINT_FREQ,
     )
     model_ln = train_loop(
         sea_creatures_split[0],
@@ -26,6 +54,19 @@ def main():
         sea_creatures_split[1],
         sea_creatures_split[4],
         "logit-normalization",
+        num_classes=NUM_CLASSES,
+        model_name=NAME,
+        num_layers=LAYERS,
+        num_wide_layers=WIDE_LAYERS,
+        droprate=DROPRATE,
+        lr=LR,
+        decay=DECAY,
+        optimizer_momentum=MOMENTUM,
+        nesterov=NESTEROV,
+        lognorm_temperature=LOGNORM_TEMP,
+        batch_size=BATCH_SIZE,
+        epochs=EPOCHS,
+        print_freq=PRINT_FREQ,
     )
 
 
--- train.py
+++ train.py
@@ -13,81 +13,146 @@ import torch.utils.data
 import torchvision.transforms as transforms
 from models import WideResNet
 
-EPOCHS = 200
-BATCH_SIZE = 128
-LR = 0.1
-MOMENTUM = 0.9
-NESTEROV = True
-DECAY = 5e-4
-PRIT_FREQ = 10
-LAYERS = 40
-WIDE_LAYERS = 2
-DROPRATE = 0
-NAME = "WideResNet-40-2"
-NUM_CLASSES = 22
 TRANS = transform_train = transforms.Compose([transforms.ToTensor()])
 
 # https://github.com/hongxin001/logitnorm_ood/blob/main/common/loss_function.py logit norm loss
 
 
 class LogitNormLoss(nn.Module):
-    def __init__(self, t=1.0):
+    """
+    Class for Logit Normalization Loss.
+
+    Inherits from nn.Module.
+    """
+
+    def __init__(self, t: float = 1.0) -> None:
+        """
+        Constructor for the LogitNormLoss class.
+
+        Args:
+            t (float): a temperature parameter that modulates the magnitude of the logits.
+
+        """
         super(LogitNormLoss, self).__init__()
         self.t = t
 
-    def forward(self, x, target):
+    def forward(self, x: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """
+        Computes the Logit Normalization Loss between an output and a target.
+
+        Arg:
+            x (torch.Tensor): output tensor.
+            target (torch.Tensor): target output tensor.
+
+        Returns:
+            torch.Tensor: the Logit Normalization Loss between the output and the target.
+        """
         norms = torch.norm(x, p=2, dim=-1, keepdim=True) + 1e-7
         logit_norm = torch.div(x, norms) / self.t
         return F.cross_entropy(logit_norm, target)
 
 
 class ImageDataset(torch.utils.data.Dataset):
+    """
+    Class for Logit Normalization Loss.
+
+    Inherits from torch.utils.data.Dataset.
+    """
+
     def __init__(self, images: np.ndarray, labels: np.ndarray) -> None:
+        """
+        Constructor for an ImageDataset class.
+
+        Args:
+            images (np.ndarray): an array with images.
+            labels (np.ndarray): an array with labels corresponding to the images.
+        """
         self.images = images
         self.labels = labels
 
-    def __len__(self):
-        """Return the number of samples in the dataset."""
+    def __len__(self) -> int:
+        """
+        Gives the length of the dataset.
+
+        Returns:
+            int: the number of samples in the dataset.
+        """
         return len(self.labels)
 
-    def __getitem__(self, idx: int) -> tuple:
-        """Given an index, return the token ids and label for the corresponding sample."""
-        # Turn image from HxWxC to CxHxW
-        item = TRANS(self.images[idx])
+    def __getitem__(self, idx: int) -> tuple[torch.Tensor, int]:
+        """
+        Gives the token ids and label for a sample at a given index.
 
-        label = self.labels[idx]  # 0 negative, 1 positive
+        Args:
+            idx (int): the index of the wanted sample.
+
+        Returns:
+            tuple[torch.Tensor, int]: a tuple with the image and label at index idx in the dataset.
+        """
+        item = TRANS(self.images[idx])
+        label = self.labels[idx]
         return item, label
 
 
 def train_loop(
-    train_images, train_labels, validation_images, validation_labels, loss="CE"
-):
-    # Data loading code
+    train_images: np.ndarray,
+    train_labels: np.ndarray,
+    validation_images: np.ndarray,
+    validation_labels: np.ndarray,
+    num_classes: int = 22,
+    model_name: str = "model",
+    num_layers: int = 40,
+    num_wide_layers: int = 2,
+    droprate: float = 0,
+    lr: float = 0.1,
+    decay: float = 5e-4,
+    optimizer_momentum: float = 0.9,
+    nesterov: bool = True,
+    loss: str = "cross-entropy",
+    lognorm_temperature: float = 1,
+    batch_size: int = 128,
+    epochs: int = 200,
+    print_freq: int = 10,
+) -> WideResNet:
+    """
+    A function for initialization, training, and evaluation on validation dataset.
+
+    Args:
+        train_images(np.ndarray): the images in the training dataset.
+        train_labels (np.ndarray): the corresponding labels of the images in the training dataset.
+        validation_images(np.ndarray): the images in the validation dataset.
+        validation_labels (np.ndarray): the corresponding labels of the images in the validation dataset.
+        loss (str): the loss to be used in the training of the model.
+
+    Returns:
+        WideResNet: the trained model
+    """
+    # Load the data.
     best_prec1 = 0
     kwargs = {"num_workers": 1, "pin_memory": True}
 
     train_loader = torch.utils.data.DataLoader(
         ImageDataset(train_images, train_labels),
-        batch_size=BATCH_SIZE,
+        batch_size=batch_size,
         shuffle=True,
         **kwargs,
     )
     val_loader = torch.utils.data.DataLoader(
         ImageDataset(validation_images, validation_labels),
-        batch_size=BATCH_SIZE,
+        batch_size=batch_size,
         shuffle=True,
         **kwargs,
     )
 
-    # create model
+    # Create the model.
     model = WideResNet(
-        LAYERS,
-        NUM_CLASSES,
-        WIDE_LAYERS,
-        dropRate=DROPRATE,
+        num_layers,
+        num_classes,
+        num_wide_layers,
+        dropRate=droprate,
     )
 
-    # get the number of model parameters
+    # Get the number of model parameters.
     print(
         "Number of model parameters: {}".format(
             sum([p.data.nelement() for p in model.parameters()])
@@ -96,36 +161,36 @@ def train_loop(
 
     cudnn.benchmark = True
 
-    # define loss function (criterion) and optimizer
+    # Define loss function (criterion) and optimizer.
     if loss == "cross-entropy":
         criterion = nn.CrossEntropyLoss()
     elif loss == "logit-normalization":
-        criterion = LogitNormLoss()
+        criterion = LogitNormLoss(lognorm_temperature)
     else:
         raise ValueError(
             f'Loss should be one of "corss-entropy" or "logit-normalization" but {loss} was given'
         )
     optimizer = torch.optim.SGD(
         model.parameters(),
-        LR,
-        momentum=MOMENTUM,
-        nesterov=NESTEROV,
-        weight_decay=DECAY,
+        lr,
+        momentum=optimizer_momentum,
+        nesterov=nesterov,
+        weight_decay=decay,
     )
 
-    # cosine learning rate
+    # Create the learning rate scheduler.
     scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
-        optimizer, len(train_loader) * EPOCHS
+        optimizer, len(train_loader) * epochs
     )
 
-    for epoch in range(EPOCHS):
-        # train for one epoch
-        train(train_loader, model, criterion, optimizer, scheduler, epoch)
+    for epoch in range(epochs):
+        # Train the model for one epoch.
+        train(train_loader, model, criterion, optimizer, scheduler, epoch, print_freq)
 
-        # evaluate on validation set
-        prec1 = validate(val_loader, model, criterion, epoch)
+        # Evaluate the model on the validation set.
+        prec1 = validate(val_loader, model, criterion, epoch, print_freq)
 
-        # remember best prec@1 and save checkpoint
+        # Remember best prec@1 and save checkpoint.
         is_best = prec1 > best_prec1
         best_prec1 = max(prec1, best_prec1)
         save_checkpoint(
@@ -136,46 +201,63 @@ def train_loop(
             },
             is_best,
             loss=loss,
+            model_name=model_name,
         )
     print("Best accuracy: ", best_prec1)
 
     return model
 
 
-def train(train_loader, model, criterion, optimizer, scheduler, epoch):
-    """Train for one epoch on the training set"""
+def train(
+    train_loader: torch.utils.data.DataLoader,
+    model: WideResNet,
+    criterion: nn.Module,
+    optimizer: torch.optimizer.SGD,
+    scheduler: torch.optim.lr_scheduler.CosineAnnealingLR,
+    epoch: int,
+    print_freq: int,
+) -> None:
+    """
+    Train for one epoch on the training dataset.
+
+    Args:
+        train_loader (torch.utils.data.DataLoader): the data loader with the images and true labels.
+        model (WideResNet): the model to be trained.
+        criterion (nn.Module): the loss to be use for the training.
+        optimizer (torch.optimizer.SGD): the optimizer to be use for the training.
+        scheduler (torch.optim.lr_scheduler.CosineAnnealingLR): the learnig rate scheduler to be use for the training.
+        epoch (int): the number of the current epoch.
+        print_freq (int): the number of epochs between printing results.
+    """
     batch_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
 
-    # switch to train mode
+    # Switch to train mode.
     model.train()
 
     end = time.time()
     for i, (inp, target) in enumerate(train_loader):
-        # target = target.cuda(non_blocking=True)
-        # input = input.cuda(non_blocking=True)
-
-        # compute output
+        # Compute output.
         output = model(inp)
         loss = criterion(output, target)
 
-        # measure accuracy and record loss
+        # Measure accuracy and record loss.
         prec1 = accuracy(output.data, target, topk=(1,))[0]
         losses.update(loss.data.item(), inp.size(0))
         top1.update(prec1.item(), inp.size(0))
 
-        # compute gradient and do SGD step
+        # Compute gradient and do SGD step.
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         scheduler.step()
 
-        # measure elapsed time
+        # Measure elapsed time.
         batch_time.update(time.time() - end)
         end = time.time()
 
-        if i % PRIT_FREQ == 0:
+        if i % print_freq == 0:
             print(
                 "Epoch: [{0}][{1}/{2}]\t"
                 "Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t"
@@ -191,35 +273,47 @@ def train(train_loader, model, criterion, optimizer, scheduler, epoch):
             )
 
 
-def validate(val_loader, model, criterion, epoch):
-    """Perform validation on the validation set"""
+def validate(
+    val_loader: torch.utils.data.DataLoader,
+    model: WideResNet,
+    criterion: nn.Module,
+    epoch: int,
+    print_freq: int,
+) -> None:
+    """
+    Perform validation on the validation dataset.
+
+    Args:
+        val_loader (torch.utils.data.DataLoader): the data loader with the images and true labels.
+        model (WideResNet): the model to be evaluated.
+        criterion (nn.Module): the loss to be use for the evaluation.
+        epoch (int): the number of the current epoch.
+        print_freq (int): the number of epochs between printing results.
+    """
     batch_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
 
-    # switch to evaluate mode
+    # Switch to evaluate mode.
     model.eval()
 
     end = time.time()
     for i, (inp, target) in enumerate(val_loader):
-        # target = target.cuda(non_blocking=True)
-        # input = input.cuda(non_blocking=True)
-
-        # compute output
+        # Compute output.
         with torch.no_grad():
             output = model(inp)
         loss = criterion(output, target)
 
-        # measure accuracy and record loss
+        # Measure accuracy and record loss.
         prec1 = accuracy(output.data, target, topk=(1,))[0]
         losses.update(loss.data.item(), inp.size(0))
         top1.update(prec1.item(), inp.size(0))
 
-        # measure elapsed time
+        # Measure elapsed time.
         batch_time.update(time.time() - end)
         end = time.time()
 
-        if i % PRIT_FREQ == 0:
+        if i % print_freq == 0:
             print(
                 "Test: [{0}/{1}]\t"
                 "Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t"
@@ -234,41 +328,81 @@ def validate(val_loader, model, criterion, epoch):
 
 
 def save_checkpoint(
-    state, is_best, filename="checkpoint.pth.tar", loss="cross-entropy"
-):
-    """Saves checkpoint to disk"""
-    directory = "runs/%s/" % (NAME)
+    state: dict,
+    is_best: bool,
+    filename: str = "checkpoint.pth.tar",
+    loss: str = "cross-entropy",
+    model_name: str = "model",
+) -> None:
+    """
+    Saves checkpoint to disk.
+
+    Args:
+        state (dict): the checkpoint data to save.
+        is_best (bool): True if the data is about the epoch with the best perfromance.
+        filename (str): The file to which to save the checkpoint.
+        loss (str): The name of the loss function.
+        model_name (str): the name with which to save the model
+    """
+    directory = "runs/%s/" % (model_name)
     if not os.path.exists(directory):
         os.makedirs(directory)
     filename = directory + filename
     torch.save(state, filename)
     if is_best:
         shutil.copyfile(
-            filename, "runs/%s/" % (NAME) + "_" + (loss) + "model_best.pth.tar"
+            filename, "runs/%s/" % (model_name) + "_" + (loss) + "model_best.pth.tar"
         )
 
 
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
+class AverageMeter:
+    """
+    Computes and stores the average and current value.
+    """
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        Constructor for the AverageMeter class.
+        """
         self.reset()
 
-    def reset(self):
+    def reset(self) -> None:
+        """
+        Resets the average values to 0.
+        """
         self.val = 0
         self.avg = 0
         self.sum = 0
         self.count = 0
 
-    def update(self, val, n=1):
+    def update(self, val: float, n: int = 1) -> None:
+        """
+        Updates the average and current values
+
+        Args:
+            val (float): current value
+            n (int): the number of steps with the current value
+        """
         self.val = val
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
 
 
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
+def accuracy(
+    output: torch.Tensor, target: torch.Tensor, topk: tuple = (1,)
+) -> list[float]:
+    """
+    Computes the precision@k for the specified values of k.
+
+    Args:
+        output (torch.Tensor): the outputed predictions of the model.
+        target (torch.Tensor): the target labels.
+        topk (tuple): tuple of k values
+
+    Returns:
+        list[float]: the accuracy for all topk values
+    """
     maxk = max(topk)
     batch_size = target.size(0)
 
@@ -281,7 +415,3 @@ def accuracy(output, target, topk=(1,)):
         correct_k = correct[:k].view(-1).float().sum(0)
         res.append(correct_k.mul_(100.0 / batch_size))
     return res
-
-
-if __name__ == "__main__":
-    train_loop()
